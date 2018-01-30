9365f66e575ca8656b7c2466b293fab5dde6ae1b
Sirajuddin Asjad <sirasjad@gmail.com>
2018-01-30 19:10:33 +0100

    Update

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import os, signal, gi, webbrowser, urllib.request, json
-import subprocess as s
+import os, signal, gi, webbrowser, urllib.request, json, subprocess as s
 gi.require_version('Gtk', '3.0');
 gi.require_version('AppIndicator3', '0.1');
 from gi.repository import Gtk, AppIndicator3
 
-currpath = os.path.dirname(os.path.realpath(__file__));
-
 class Indicator():
     def __init__(self):
         self.app = 'show_proc';
-        iconpath = currpath + '/img/btc_small.png';
+        self.currpath = os.path.dirname(os.path.realpath(__file__));
+        iconpath = self.currpath + '/img/btc_small.png';
 
         self.ind = AppIndicator3.Indicator.new(self.app, iconpath, AppIndicator3.IndicatorCategory.OTHER);
+        self.ind.set_label('BTC: $' + self.price(), self.app);
         self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE);
         self.ind.set_menu(self.create_menu());
-        self.ind.set_label('BTC: $' + self.price(), self.app);
 
     def create_menu(self):
         menu = Gtk.Menu();
@@ -39,9 +37,9 @@ class Indicator():
             return str(round(btc_usd));
 
     def priceAlert(self, price):
-        iconpath = currpath + '/img/btc_large.png';
-        minAlert = 8000;
-        maxAlert = 10000;
+        iconpath = self.currpath + '/img/btc_large.png';
+        minAlert = 10000;
+        maxAlert = 12000;
 
         if(maxAlert <= price):
             s.call(['notify-send', '-i', iconpath, 'Bitcoin Price Indicator', ('Hurray! Bitcoin price has reached $%s.' % round(price))]);
