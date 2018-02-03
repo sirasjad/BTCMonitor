f5c5cd2b0da51991f7704290743556f089a3b997
Sirajuddin Asjad <sirasjad@gmail.com>
2018-02-04 00:41:03 +0100

    Fixed label, added auto-refresh

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os, signal, gi, webbrowser, urllib.request, json, subprocess as s
+import os, signal, gi, webbrowser, urllib.request, json, time, subprocess as s
 gi.require_version('Gtk', '3.0');
 gi.require_version('AppIndicator3', '0.1');
-from gi.repository import Gtk, AppIndicator3
+from gi.repository import Gtk, AppIndicator3, GObject
+from threading import Thread
 
 class Indicator():
     def __init__(self):
@@ -10,9 +11,13 @@ class Indicator():
         iconpath = self.currpath + '/img/btc_small.png';
 
         self.ind = AppIndicator3.Indicator.new(self.app, iconpath, AppIndicator3.IndicatorCategory.OTHER);
-        self.ind.set_label('BTC: $' + self.price(), self.app);
+        self.ind.set_label(self.price(), self.app);
         self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE);
         self.ind.set_menu(self.create_menu());
+
+        self.update = Thread(target = self.refreshPrice);
+        self.update.setDaemon(True);
+        self.update.start();
 
     def create_menu(self):
         menu = Gtk.Menu();
@@ -34,12 +39,18 @@ class Indicator():
             data = json.loads(url.read().decode());
             btc_usd = data['USD']['last'];
             self.priceAlert(btc_usd);
-            return str(round(btc_usd));
+            label = 'BTC: $' + str(round(btc_usd));
+            return label;
+
+    def refreshPrice(self):
+        while True:
+            time.sleep(300); # Update label after 5 minutes
+            GObject.idle_add(self.ind.set_label, self.price(), self.app, priority = GObject.PRIORITY_DEFAULT);
 
     def priceAlert(self, price):
         iconpath = self.currpath + '/img/btc_large.png';
-        minAlert = 10000;
-        maxAlert = 12000;
+        minAlert = 9000;
+        maxAlert = 11000;
 
         if(maxAlert <= price):
             s.call(['notify-send', '-i', iconpath, 'Bitcoin Price Indicator', ('Hurray! Bitcoin price has reached $%s.' % round(price))]);
@@ -53,5 +64,6 @@ class Indicator():
         Gtk.main_quit();
 
 Indicator();
+GObject.threads_init();
 signal.signal(signal.SIGINT, signal.SIG_DFL);
 Gtk.main();
